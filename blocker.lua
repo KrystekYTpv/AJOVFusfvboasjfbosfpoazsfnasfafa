147473393d9b7350987f4a5228dda857bca4e44c
KrystekYTpv <93543150+KrystekYTpv@users.noreply.github.com>
2021-11-02 17:46:53 +0100

    Create blocker.lua

---
+++ blocker.lua
@@ -0,0 +1,15 @@
+local links = {
+    'https://raw.githubusercontent.com/KrystekYTpv/PSXpv/main/WhiteListKody.lua', --whitelisted osoby,
+    'https://raw.githubusercontent.com/KrystekYTpv/PSXpv/main/psxScriptFree.lua', --skrypt jezeli free wersja,
+    'https://raw.githubusercontent.com/KrystekYTpv/PSXpv/main/psxScriptPremium.lua', --skrypt jezeli premium wersja
+}
+
+local old 
+old = hookfunction(game.HttpGet, function(self, url, ...)
+    for i,v in pairs(links) do 
+        if url == v then 
+            return 'skid'
+        end 
+    end
+    return old(self, url, ...)
+end) 
