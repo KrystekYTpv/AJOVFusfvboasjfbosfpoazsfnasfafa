63a157ada920c19205669aa523c3a30c251099c5
KrystekYTpv <93543150+KrystekYTpv@users.noreply.github.com>
2021-11-02 22:10:18 +0100

    Create test1.lua

---
+++ test1.lua
@@ -0,0 +1,15 @@
+wait()
+
+local check = loadstring(game:HttpGet('https://raw.githubusercontent.com/KrystekYTpv/PSXpv/main/WhiteListKody.lua', true))
+local free = loadstring(game:HttpGet('https://raw.githubusercontent.com/KrystekYTpv/PSXpv/main/psxScriptFree.lua'))
+local premium = loadstring(game:HttpGet('https://raw.githubusercontent.com/KrystekYTpv/PSXpv/main/psxScriptPremium.lua'))
+
+if table.find(check(), tostring(game.Players.LocalPlayer.UserId)) and wait(1) then 
+    print('Premium', check)
+    require(game:GetService("ReplicatedStorage").Framework.Modules.Client["5 | Message"]).New("Loading Premium Version! \nClick Ok!")
+    premium()
+else
+    print('Free', check)
+    require(game:GetService("ReplicatedStorage").Framework.Modules.Client["5 | Message"]).New("Loading Free Version! \nClick Ok!")
+    free()
+end
