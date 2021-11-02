65534f5e43060f378c27116ceb550094028b895e
KrystekYTpv <93543150+KrystekYTpv@users.noreply.github.com>
2021-11-02 19:41:09 +0100

    Update WhiteListKody.lua

--- WhiteListKody.lua
+++ WhiteListKody.lua
@@ -1,39 +1,39 @@
 
---local whitelisted = {
---'320664513', -- KrystekYT
---'294992884', -- DjRudzik
---'2898280324', -- Weronika9896
---'1764348997', -- Kacpero202
---'639977402', -- DawajSernik
---'531348383', -- Wariatka1337
---'1100409110', -- Polska_Kielbaska
---'',
---'',
---'',
---'',
---'',
---'',
---}
---return whitelisted
+local whitelisted = {
+'320664513', -- KrystekYT
+'294992884', -- DjRudzik
+'2898280324', -- Weronika9896
+'1764348997', -- Kacpero202
+'639977402', -- DawajSernik
+'531348383', -- Wariatka1337
+'1100409110', -- Polska_Kielbaska
+'',
+'',
+'',
+'',
+'',
+'',
+}
+return whitelisted
 
 
 ---------------------------------------------------------------
 --[USERIDHERE] = true - wlacza dostep
 --[USERIDHERE] = false - wylacza dostep
 --
-userids = {
-[320664513] = true, -- KrystekYT
-[294992884] = true, -- DjRudzik
-[2898280324] = true, -- Weronika9896
-[1764348997] = true, -- Kacpero202
-[639977402] = true, -- DawajSernik
-[531348383] = true, -- Wariatka1337
-[1100409110] = true, -- Polska_Kielbaska
-[] = true, --
-[] = true, -- 
-[] = true, -- 
-[] = true, -- 
-[] = true, -- 
-[] = true, -- 
-}
-return userids
+--userids = {
+--[320664513] = true, -- KrystekYT
+--[294992884] = true, -- DjRudzik
+--[2898280324] = true, -- Weronika9896
+--[1764348997] = true, -- Kacpero202
+--[639977402] = true, -- DawajSernik
+--[531348383] = true, -- Wariatka1337
+--[1100409110] = true, -- Polska_Kielbaska
+--[] = true, --
+--[] = true, -- 
+--[] = true, -- 
+--[] = true, -- 
+--[] = true, -- 
+--[] = true, -- 
+--}
+--return userids
