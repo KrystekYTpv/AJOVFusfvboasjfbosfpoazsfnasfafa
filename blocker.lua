8babb514731566deab1e174bcdaeb433254ca83b
KrystekYTpv <93543150+KrystekYTpv@users.noreply.github.com>
2021-11-02 17:47:32 +0100

    Update blocker.lua

--- blocker.lua
+++ blocker.lua
@@ -2,6 +2,7 @@ local links = {
     'https://raw.githubusercontent.com/KrystekYTpv/PSXpv/main/WhiteListKody.lua', --whitelisted osoby,
     'https://raw.githubusercontent.com/KrystekYTpv/PSXpv/main/psxScriptFree.lua', --skrypt jezeli free wersja,
     'https://raw.githubusercontent.com/KrystekYTpv/PSXpv/main/psxScriptPremium.lua', --skrypt jezeli premium wersja
+    'https://raw.githubusercontent.com/KrystekYTpv/PSXpv/main/blocker.lua', --blocker
 }
 
 local old 
