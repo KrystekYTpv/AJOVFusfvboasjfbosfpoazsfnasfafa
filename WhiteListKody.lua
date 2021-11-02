2edf4637700ac0c5686ac07d8cf3b7f15aba4b94
KrystekYTpv <93543150+KrystekYTpv@users.noreply.github.com>
2021-11-02 23:05:56 +0100

    Update WhiteListKody.lua

--- WhiteListKody.lua
+++ WhiteListKody.lua
@@ -1,20 +1,4 @@
----------------------------------------------------------------
---[USERIDHERE] = true - wlacza dostep
---[USERIDHERE] = false - wylacza dostep
-
-userids = {
-[320664513] = true, -- KrystekYT
-[294992884] = true, -- DjRudzik
-[2898280324] = true, -- Weronika9896
-[1764348997] = true, -- Kacpero202
-[639977402] = true, -- DawajSernik
-[531348383] = true, -- Wariatka1337
-[1100409110] = true, -- Polska_Kielbaska
-[] = true, --
-[] = true, -- 
-[] = true, -- 
-[] = true, -- 
-[] = true, -- 
-[] = true, -- 
+local whitelisted = {
+'320664513',
 }
-return userids
+return whitelisted
