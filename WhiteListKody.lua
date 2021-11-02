ed7063abc93707b0541661c56d84e271d46b24b9
KrystekYTpv <93543150+KrystekYTpv@users.noreply.github.com>
2021-11-02 23:11:47 +0100

    Update WhiteListKody.lua

--- WhiteListKody.lua
+++ WhiteListKody.lua
@@ -1,4 +1,17 @@
-local whitelisted = {
-'320664513',
+
+userids = {
+[320664513] = true, -- KrystekYT
+[294992884] = true, -- DjRudzik
+[2898280324] = true, -- Weronika9896
+[1764348997] = true, -- Kacpero202
+[639977402] = true, -- DawajSernik
+[531348383] = true, -- Wariatka1337
+[1100409110] = true, -- Polska_Kielbaska
+[] = true, --
+[] = true, -- 
+[] = true, -- 
+[] = true, -- 
+[] = true, -- 
+[] = true, -- 
 }
-return whitelisted
+return userids
