b93e98e19278b7ac35e4463650a9bdea4b39c1d7
KrystekYTpv <93543150+KrystekYTpv@users.noreply.github.com>
2021-11-02 22:43:37 +0100

    Update WhiteListKody.lua

--- WhiteListKody.lua
+++ WhiteListKody.lua
@@ -1,14 +1,13 @@
 
 local whitelisted = {
 '320664513', -- KrystekYT
-'294992884', -- DjRudzik
 '2898280324', -- Weronika9896
 '1764348997', -- Kacpero202
 '639977402', -- DawajSernik
 '531348383', -- Wariatka1337
 '1100409110', -- Polska_Kielbaska
 '1068992223', -- tutaccount
---'',
+'294992884',
 --'',
 --'',
 --'',
