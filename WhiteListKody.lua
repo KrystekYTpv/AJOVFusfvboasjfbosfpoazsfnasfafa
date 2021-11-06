7a9ab8da5dad70371e1c860c127bd86fb786def3
KrystekYTpv <93543150+KrystekYTpv@users.noreply.github.com>
2021-11-06 17:15:39 +0100

    Update WhiteListKody.lua

--- WhiteListKody.lua
+++ WhiteListKody.lua
@@ -1,27 +1,27 @@
-userids = {
-[320664513] = true, -- KrystekYT
-[294992884] = true, -- DjRudzik
-[2898280324] = true, -- Weronika9896
-[1764348997] = true, -- Kacpero202
-[639977402] = true, -- DawajSernik
-[531348383] = true, -- Wariatka1337
-[1100409110] = true, -- Polska_Kielbaska
-[1068992223] = true, -- tutaccount
-[2433227342] = true, -- KubaGames2115
-[2854035659] = true, -- NobekWloluu
-[652382788] = true, -- lewowity
-[734725590] = true, -- WiktorGames32
-[761017630] = true, -- bobejlk
-[2864331664] = true, -- PolCher1
-[2939661997] = true, -- Fan_Yuujina
-[2624963365] = true, -- Hnea1338v2
-[1173490265] = true, -- xykacper
-[1038425504] = true, -- KubaJeden337
-[720781459] = true, -- Kuraworkout123
-[1038290884] = true, -- tymowie
-[473416850] = true, -- JedrekGir4
-[417336375] = true, -- MrKoxDominik
-[2866274812] = true, -- a
+
+local premiumList = {
+  [320664513] = "KrystekYT", -- KrystekYT
+  [294992884] = "DjRudzik", -- DjRudzik
+  [2898280324] = "Weronika9896", -- Weronika9896
+  [1764348997] = "Kacpero202", -- Kacpero202
+  [639977402] = "DawajSernik", -- DawajSernik
+  [531348383] = "Wariatka1337", -- Wariatka1337
+  [1100409110] = "Polska_Kielbaska", -- Polska_Kielbaska
+  [1068992223] = "tutaccount", -- tutaccount
+  [2433227342] = "KubaGames2115", -- KubaGames2115
+  [2854035659] = "NobekWloluu", -- NobekWloluu
+  [652382788] = "lewowity", -- lewowity
+  [734725590] = "WiktorGames32", -- WiktorGames32
+  [761017630] = "bobejlk", -- bobejlk
+  [2864331664] = "PolCher1", -- PolCher1
+  [2939661997] = "Fan_Yuujina", -- Fan_Yuujina
+  [2624963365] = "Hnea1338v2", -- Hnea1338v2
+  [1173490265] = "xykacper", -- xykacper
+  [1038425504] = "KubaJeden337", -- KubaJeden337
+  [720781459] = "Kuraworkout123", -- Kuraworkout123
+  [1038290884] = "tymowie", -- tymowie
+  [473416850] = "JedrekGir4", -- JedrekGir4
+  [417336375] = "MrKoxDominik", -- MrKoxDominik
+  [2866274812] = "OrderClaim3"
 }
-return userids
-----
+return premiumList
