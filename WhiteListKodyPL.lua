10f2c6c6e6b289b93a68ece020f2e71075d31f19
KrystekYTpv <93543150+KrystekYTpv@users.noreply.github.com>
2022-01-08 19:42:22 +0100

    Create WhiteListKodyPL.lua

---
+++ WhiteListKodyPL.lua
@@ -0,0 +1,10 @@
+local premiumPL = {
+    [1028874214] = 'klocek270811',
+    [3090409883] = 'Rocznik1958',
+    [2817180463] = 'HeyGetBack2',
+    [3117371652] = 'NobekJestemX',
+    [421626382] = 'kopycho',
+    [1764348997] = 'Kacpero202',
+    --[] = '',
+}
+return premiumPL
