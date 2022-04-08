fac4f6f21a535ebe68d419da301b6a1f888e6871
KrystekYTpv <93543150+KrystekYTpv@users.noreply.github.com>
2022-04-08 20:24:06 +0200

    Update ASLoajbfasbfoasfa.lua

--- ASLoajbfasbfoasfa.lua
+++ ASLoajbfasbfoasfa.lua
@@ -1,16 +1,14 @@
 local donatorList = loadstring(game:HttpGet('https://raw.githubusercontent.com/KrystekYTpv/AJOVFusfvboasjfbosfpoazsfnasfafa/main/donatorList.lua', true))()
     
-if iconpetdeleter then
+if donatorList[game.Players.LocalPlayer.UserId] and donatorList[game.Players.LocalPlayer.UserId] == game.Players.LocalPlayer.Name then
     getgenv().StartPetIconDeleter = true
-    if donatorList[game.Players.LocalPlayer.UserId] and donatorList[game.Players.LocalPlayer.UserId] == game.Players.LocalPlayer.Name then
-        while wait(1) and StartPetIconDeleter do
-            for i,v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Inventory.Frame.Main.Pets:GetDescendants()) do
-                if v.Name == "PetIcon" then
-                    v:Destroy()
-                end
+    while wait(1) and StartPetIconDeleter do
+        for i,v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Inventory.Frame.Main.Pets:GetDescendants()) do
+            if v.Name == "PetIcon" then
+                v:Destroy()
             end
         end
-    else
-        require(game:GetService("ReplicatedStorage").Framework.Modules.Client["5 | Message"]).New("Sorry you have to buy Donator from: \n!KrystekYT#8687")
     end
+else
+    require(game:GetService("ReplicatedStorage").Framework.Modules.Client["5 | Message"]).New("Sorry you have to buy Donator from: \n!KrystekYT#8687")
 end
