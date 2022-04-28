aae7b45752720f65ce3b466c60f486ef6ffd402e
KrystekYTpv <93543150+KrystekYTpv@users.noreply.github.com>
2022-04-28 16:40:11 +0200

    Update ASLoajbfasbfoasfa.lua

--- ASLoajbfasbfoasfa.lua
+++ ASLoajbfasbfoasfa.lua
@@ -1,20 +1,15 @@
+local hwidListDonator = loadstring(game:HttpGet('https://raw.githubusercontent.com/KrystekYTpv/AJOVFusfvboasjfbosfpoazsfnasfafa/main/hwidListDonator.lua', true))()
+local HwidCheck = tostring(game:GetService("RbxAnalyticsService"):GetClientId())
 
-    if getgenv().StartPetIconDelete == true then
-        StartPetIconDeleter = true
-        while wait(1) and StartPetIconDeleter == true do
+if table.find(hwidListDonator,HwidCheck) then
+    StartPetIconDeleter = true
+    while wait(1) and StartPetIconDeleter == true do
             for i,v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Inventory.Frame.Main.Pets:GetDescendants()) do
                 if v.Name == "PetIcon" then
                     v.Visible = false
                 end
             end
         end
-    elseif getgenv().StartPetIconDelete == false then
-        StartPetIconDeleter = false
-        while wait(1) and StartPetIconDeleter == false do
-            for i,v in pairs(game:GetService("Players").LocalPlayer.PlayerGui.Inventory.Frame.Main.Pets:GetDescendants()) do
-                if v.Name == "PetIcon" then
-                    v.Visible = true
-                end
-            end
-        end
-    end
+else
+    require(game:GetService("ReplicatedStorage").Framework.Modules.Client["5 | Message"]).New("Sorry you have to buy Donator from: \n!KrystekYT#8687")
+end
