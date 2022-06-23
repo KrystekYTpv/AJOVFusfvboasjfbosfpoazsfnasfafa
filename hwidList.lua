5a7f7a9eaa8f36dc6cec478f94dd88334369f9ab
KrystekYTpv <93543150+KrystekYTpv@users.noreply.github.com>
2022-06-24 00:17:04 +0200

    Update hwidList.lua

--- hwidList.lua
+++ hwidList.lua
@@ -56,7 +56,7 @@ local hwidList = {
     '9CCA33D3-CB98-4BE9-A8C1-16822D349E9D'; --Kamilekox#4334
     'F0767C9A-B70E-4133-A94A-CE36EFFAF678'; --Czikach#9725
     'A7B9B1FF-D901-4997-B9E3-C91DA48537B2'; --ˡⁱˡᶜʳᵃᶜᵏᵉʳღ#3370
-    --''; --
+    '476CA3D0-6417-47D2-A825-F954DCAA81D1'; --Pandison#5114
     --''; --
     --''; --
     --''; --
