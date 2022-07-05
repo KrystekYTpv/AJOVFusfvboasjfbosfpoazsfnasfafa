a8f7578b643ef22da69120f39bc3352e5cb917a0
KrystekYTpv <93543150+KrystekYTpv@users.noreply.github.com>
2022-07-06 00:10:02 +0200

    Update hwidListPosPet.lua

--- hwidListPosPet.lua
+++ hwidListPosPet.lua
@@ -1,6 +1,6 @@
 local hwidListPosPet = {
-    --''; --
-    --''; --
+    'E522BBE6-5E60-4FFD-B5CC-3850020C7167'; -- Ja
+    'C4A2B623-5BD1-4E02-A9D8-383DCC9ABAF0'; -- Gigant#0001
     --''; --
     --''; --
     --''; --
