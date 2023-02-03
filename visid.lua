d1e3c7fa2c9938ab58642cb50651a07f702453cd
KrystekYTpv <93543150+KrystekYTpv@users.noreply.github.com>
2023-02-03 20:38:52 +0100

    Update visid.lua

--- visid.lua
+++ visid.lua
@@ -4,7 +4,7 @@ local visid = {
     '9A7A3F97-373D-41A5-BF1E-087689C2B778'; -- MjuziQu <3#3324
     '506D3F34-E1AF-4F81-9899-D1B97760DE8E'; -- Zungg#1944
     '915ABB12-6C84-4C32-8AFE-7C1494EBF09A'; -- ThisIsMago#9973
-    --''; --
+    '3043C5A2-D1FE-49E9-A9F5-A68363AAEA9F'; -- Cipa TM
     --''; --
     --''; --
     --''; --
