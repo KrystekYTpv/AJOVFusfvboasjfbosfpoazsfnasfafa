d6ea1caa8f014bafaa1f98703029896ccfc1a783
KrystekYTpv <93543150+KrystekYTpv@users.noreply.github.com>
2023-04-22 13:53:12 +0200

    Update hwidList.lua

--- hwidList.lua
+++ hwidList.lua
@@ -1,11 +1,21 @@
 local hwidList = {
-    {
+    { -- 419834999991304213
         id = 'E522BBE6-5E60-0C7167A1',
         expirationDate = {
             year = 2023,
-         month = 5,
+            month = 5,
             day = 1
         }
     },
+    
+    --{
+    --    id = 'E522BBE6-5E60-0C7167A1',
+    --    expirationDate = {
+    --        year = 2023,
+    --        month = 5,
+    --        day = 1
+    --    }
+    --},
+    
 }
 return hwidList
