969a09588b38cea39f4b2cac3ba487b3e496b496
KrystekYTpv <93543150+KrystekYTpv@users.noreply.github.com>
2023-04-22 14:19:35 +0200

    Update hwidList.lua

--- hwidList.lua
+++ hwidList.lua
@@ -8,6 +8,15 @@ local hwidList = {
         }
     },
     
+    {
+        id = 'BE213E62-4251-4340-B3A7-E7C6B1B3A199',
+        expirationDate = {
+            year = 2023,
+            month = 5,
+            day = 22
+        }
+    },
+    
     --{
     --    id = 'E522BBE6-5E60-0C7167A1',
     --    expirationDate = {
